e1d8961e19fd4cc161f45c8df933430724fc9c97
debbykt <debbytampubolon24@gmail.com>
2026-03-11 19:22:28 -0700

    inisialisasi project modul 4

---
+++ tpmodul4_103082400029/KodePos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103082400029
+{
+    internal class KodePos
+    {
+        private Dictionary<string, int> kodePosTable = new Dictionary<string, int>()
+        {
+            {"Batununggal", 40266},
+            {"Kujangsari", 40287},
+            {"Mengger", 40267},
+            {"Wates", 40256},
+            {"Cijaura", 40287},
+            {"Jatisari", 40286},
+            {"Margasari", 40286},
+            {"Sekejati", 40286},
+            {"Kebonwaru", 40272},
+            {"Maleer", 40274},
+            {"Samoja", 40273}
+        };
+
+        public int getKodePos(string kelurahan)
+        {
+            if (kodePosTable.ContainsKey(kelurahan))
+            {
+                return kodePosTable[kelurahan];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ tpmodul4_103082400029/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103082400029
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kode = new KodePos();
+
+            Console.Write("Masukkan kelurahan : ");
+            string kelurahan = Console.ReadLine();
+
+            int hasil = kode.getKodePos(kelurahan);
+
+            if (hasil != -1)
+            {
+                Console.WriteLine("Kode Pos : " + hasil);
+            }
+            else
+            {
+                Console.WriteLine("Kelurahan tidak ditemukan");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
